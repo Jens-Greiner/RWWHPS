016fc779ecda798dad0c68622db3944112effa66
Jens Greiner <uwkzx@student.kit.edu>
2022-06-09 21:49:09 +0200

    added beta distrubution

---
+++ Files/src/main/values.js
@@ -0,0 +1,7 @@
+//uses a beta distribution
+function rngBetaDistr(){
+    var unif = Math.random();//uniform distribution
+            var beta = Math.pow(Math.sin(unif * Math.PI / 2),2);
+            beta_left = (beta < 0.5) ? 2 * beta : 2 * (1 - beta);//skewing beta towards leftside
+            console.log(beta_left);
+}
